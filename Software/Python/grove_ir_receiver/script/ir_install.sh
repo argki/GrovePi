8b374504dbe747c3deb04b77e688731bd358e766
argki <k.info.idea@gmail.com>
2025-12-14 00:53:09 +0900

    chore: drop python2 support and use python3 only

--- Software/Python/grove_ir_receiver/script/ir_install.sh
+++ Software/Python/grove_ir_receiver/script/ir_install.sh
@@ -30,7 +30,7 @@ echo " "
 echo "Installing Dependencies"
 echo "======================="
 sudo apt-get update
-sudo apt-get install lirc python-lirc -y
+sudo apt-get install lirc python3-lirc -y
 
 echo " "
 echo "Copying Config Files"
